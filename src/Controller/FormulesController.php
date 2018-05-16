6ca2762370b583873d61b9637f1db9a20c4170d7
jado <tony76.200@hotmail.fr>
2018-05-16 09:33:26 +0200

    Ajout Controller Formules  et inscriptions

---
+++ src/Controller/FormulesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FormulesController extends Controller
+{
+    /**
+     * @Route("/formules", name="formules")
+     */
+    public function index()
+    {
+        return $this->render('formules/index.html.twig', [
+            'controller_name' => 'FormulesController',
+        ]);
+    }
+}
